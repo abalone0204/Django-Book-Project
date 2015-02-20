9fbdeaa778590c68fe69625d2ea643fa33be5dca
abalone0204 <abalone0204@gmail.com>
2015-02-20 21:53:49 +0800

    add hello

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello', 'hello'),
 )

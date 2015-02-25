117a30b9c80503a9d77ec6ca7cb1395f2f9f959d
abalone0204 <abalone0204@gmail.com>
2015-02-25 14:10:54 +0800

    add current time view and url

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from mysite.views import hello
+from mysite.views import current_time
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^hello', 'hello'),
+    ('^hello/$', hello),
+    ('^current_time/$', current_time),
+
 )
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import datetime
 
 def hello(request):
     return HttpResponse("hello world")
+
+def current_time(request):
+    now = datetime.datetime.now()
+    html = "<h1>Now is %s</h1>" % now
+    return HttpResponse(html)
+
